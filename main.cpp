a9873953768fb269719d576ad9a23760b4e5a0a3
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-11-16 23:58:44 +0530

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "QtWidgetsApplication2.h"
+#include <QtWidgets/QApplication>
+#include <QDebug>
+int main(int argc, char *argv[])
+{
+
+    QCoreApplication::addLibraryPath("E:/AI PRACTICAL/Qt/Qt5.12.12/5.12.12/msvc2017/plugins");
+   /* QApplication app(argc, argv);*/
+    QApplication a(argc, argv);
+    QtWidgetsApplication2 w;
+    qDebug() << "Starting application...";
+    w.show();
+    return a.exec();
+}
